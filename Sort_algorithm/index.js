2c875c642bf61f8583eafeb2cda83aa7b373643e
so-hard <qq1136339397@gmail.com>
2019-04-14 19:51:55 +0800

    add 增加一些注释

--- Sort_algorithm/index.js
+++ Sort_algorithm/index.js
@@ -5,7 +5,7 @@ class CArray {
         this.numElements = numElements;
         this.gaps = [5, 3, 1];
     }
-
+    //给CArray对象的dataStore属性赋值
     setData() {
         for (let i = 0; i < this.numElements; i++) {
             this.dataStore[i] = Math.floor(Math.random() * (1000 + 1))
@@ -13,11 +13,12 @@ class CArray {
         }
     }
 
+    //交换
     swap(arr, index1, index2) {
         [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
     }
 
-
+    //显示数组内的元素每十个换行
     show() {
         let restr = "";
         for (let i = 0; i < this.dataStore.length; i++) {
@@ -29,10 +30,11 @@ class CArray {
         return restr;
     }
 
+    // 显示排序所花费的时间
     showRunTime(fun) {
         this.setData();
         let start = new Date().getTime();
-        fun.call(nums);
+        fun.call(this);
         let stop = new Date().getTime();
         console.log(`用时${stop - start}`);
     }
@@ -66,6 +68,8 @@ class CArray {
         }
     }
 
+
+    //插入排序，每次将未排序的数组中的第一个元素来和已排序的数组来比较。
     insertion() {
         let temp, inner, outer,
             len = this.dataStore.length;
@@ -111,10 +115,32 @@ class CArray {
         }
         // console.log(h);
     }
+
+    // mergeSort(item = this.dataStore) {
+    //     if(item.length == 1){
+    //         return item;
+    //     }
+    //     let middle = Math.floor(item.length/2),
+    //         left = this.dataStore.splice(0,middle),
+    //         right = this.dataStore.splice(middle);
+    //         return this.merge(this.mergeSort(left), this.mergeSort(right));
+    // }
+
+    // merge(left, right) {
+    //     let result = [];
+    //     while(left.length > 0 && right.length > 0){
+    //         if(left[0] < right[0]){
+    //             result.push(left.shift());
+    //         }else{
+    //             result.push(right.shift());
+    //         }
+    //     }
+    //     return result.concat(left).concat(right);
+    // }
+    
 }
-var numElements = 50000;
+var numElements = 2000;
 var nums = new CArray(numElements);
-nums.showRunTime(nums.shellSortT);
 nums.showRunTime(nums.shellSort);
 // nums.shellSortT()
 
